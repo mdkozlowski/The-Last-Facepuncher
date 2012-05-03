73edbef42f03ad2663e1142c87e1748490bfcfa4
Marcin <naarkie@gmail.com>
2012-05-03 16:29:37 +0200

    [ADD] Added Level, Tile and Room classes.

---
+++ TheLastFacepuncher/TheLastFacepuncher/Level.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheLastFacepuncher
+{
+    public class Level
+    {
+        public List<Room> Rooms = new List<Room>();
+
+        public Level()
+        {
+        }
+    }
+}
---
+++ TheLastFacepuncher/TheLastFacepuncher/Room.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheLastFacepuncher
+{
+    public class Room
+    {
+        public Tile[,] Tiles = new Tile[40, 24];
+
+        public Room()
+        {
+        }
+    }
+}
